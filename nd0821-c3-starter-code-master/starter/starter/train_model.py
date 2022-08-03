2982bb6399b30c686ca80b6eb1570b4e2e6ce543
Rocky <rock.e.core@gmail.com>
2022-08-03 14:26:43 -0700

    added split_data and fit_model functions

--- nd0821-c3-starter-code-master/starter/starter/train_model.py
+++ nd0821-c3-starter-code-master/starter/starter/train_model.py
@@ -1,31 +1,45 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
+from .ml.data import process_data
+from .ml.model import train_model
+from joblib import dump
 
 # Add the necessary imports for the starter code.
 
+def split_data(data_dir):
+    # Add code to load in the data.
+    data = pd.read_csv(data_dir)
+    
+    train, test = train_test_split(data, test_size=0.20)
+    
+    return train, test
 
-# Add code to load in the data.
-census_data = "data/cencus.csv"
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+def fit_model(train, model_out):
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
 
-# Proces the test data with the process_data function.
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country"]
+    
+    # Proces the test data with the process_data function.
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True)
 
-# Train and save a model.
+    # Train and save a model.
+    model = train_model(X_train, y_train)
+    
+    # Dump it
+    dump(model, model_out)
+
+
+
